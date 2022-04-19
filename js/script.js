61cea08b441d3c6586ce572b7514b7737a9bc5b3
Betsa Meneses <betsadmd@gmail.com>
2022-04-19 19:51:30 +0200

    smooth scroll

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const sr= ScrollReveal({
 })
 
 sr.reveal('.title');
-sr.reveal('.presentation', { delay: 700 });
+sr.reveal('.presentation');
 sr.reveal('.container-CTA', { delay: 900, origin:'bottom'});
 sr.reveal('.paragraph', {delay:200, origin:'bottom'});
 
@@ -37,25 +37,25 @@ let images = Array.from(document.querySelectorAll(".img-container"));
 //Changing background color
 
 $(window).on("scroll touchmove", function() {
-    if ($(document).scrollTop() >= $("#projet01").position().top / 1.2 ) {
+    if ($(document).scrollTop() >= $("#projet01").position().top / 1.5 ) {
       $("#projet01").css('background', $("#projet01").attr("data-color"));    
     }
     else{
       $("#projet01").css('background', "#f4f4f4")
     };
-    if ($(document).scrollTop() > $("#projet02").position().top / 1.2) {
+    if ($(document).scrollTop() > $("#projet02").position().top / 1.5) {
       $("#projet02").css('background', $("#projet02").attr("data-color"))
     }
     else{
       $("#projet02").css('background', "#f4f4f4")
     };
-    if ($(document).scrollTop() > $("#projet03").position().top / 1.1) {
+    if ($(document).scrollTop() > $("#projet03").position().top / 1.4) {
       $("#projet03").css('background', $("#projet03").attr("data-color"))
     }
     else{
       $("#projet03").css('background', "#f4f4f4")
     };
-    if ($(document).scrollTop() > $("#projet04").position().top / 1.05) {
+    if ($(document).scrollTop() > $("#projet04").position().top / 1.3) {
       $("#projet04").css('background', $("#projet04").attr("data-color"))
     }
     else{
--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,21 +1,23 @@
-const body = document.body,
-scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
-height = scrollWrap.getBoundingClientRect().height - 1,
-speed = 0.04;
+// const body = document.body,
+// scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0],
+// height = scrollWrap.getBoundingClientRect().height - 1,
+// speed = 0.04;
 
-var offset = 0;
+// var offset = 0;
 
-body.style.height = Math.floor(height) + "px";
+// body.style.height = Math.floor(height) + "px";
 
-function smoothScroll() {
-offset += (window.pageYOffset - offset) * speed;
+// function smoothScroll() {
+//     offset += (window.pageYOffset - offset) * speed;
 
-var scroll = "translateY(-" + offset + "px) translateZ(0)";
-scrollWrap.style.transform = scroll;
+//     var scroll = "translateY(-" + offset + "px) translateZ(0)";
+//     scrollWrap.style.transform = scroll;
 
-callScroll = requestAnimationFrame(smoothScroll);
-}
+//     callScroll = requestAnimationFrame(smoothScroll);
+// }
 
-smoothScroll();
+// smoothScroll();
   
 //  recuerda agregar main
+
+
